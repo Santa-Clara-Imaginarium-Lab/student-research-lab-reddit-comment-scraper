44ded2a0acbfbbb4a0c62cca6812b0c078198a61
Jason Vu <jvu@scu.edu>
2021-05-07 14:57:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { CommandoClient } = require("discord.js-commando");  
 const path = require("path");     
 
@@ -16,9 +15,7 @@ const spotifyApi = new SpotifyWebApi({
     clientId: client.config.api.spotifyClientID,
     clientSecret: client.config.api.spotifyClientSecret
 });
-
-client.spotifyApi = spotifyApi; 
-
+ 
 client.registry
   .registerDefaultTypes()
   .registerGroups([   
@@ -62,71 +59,5 @@ client.once("ready", () => {
 client 
     .on("message", (message) => require("./events/message")(client, message))
     .on("guildMemberAdd", (member) => require("./events/guildMemberAdd")(client, member));
- 
-=======
-const { CommandoClient } = require("discord.js-commando");  
-const path = require("path");     
 
-const client = new CommandoClient({
-  commandPrefix: `${require("./config.json").prefix}`,
-  owner: `${require("./config.json").serverRoles.botOwner}`,
-});
-
-client.config = require("./config.json"); 
-client.error = require("./functions/error.js");
-const { log } = require("./functions/log.js");   
-
-const SpotifyWebApi = require("spotify-web-api-node");
-const spotifyApi = new SpotifyWebApi({
-    clientId: client.config.api.spotifyClientID,
-    clientSecret: client.config.api.spotifyClientSecret
-});
-
-client.spotifyApi = spotifyApi; 
-
-client.registry
-  .registerDefaultTypes()
-  .registerGroups([   
-    ["admins", "Admins"],
-    ["fun", "Fun"],
-    ["practicality", "Practicality"],
-  ])
-  .registerDefaultGroups()
-  .registerDefaultCommands({
-    unknownCommand: false,
-    prefix: false, 
-  })
-  .registerCommandsIn(path.join(__dirname, "commands")); 
-
-client.dispatcher.addInhibitor( (client, msg) => {
-  try { 
-    switch (msg.command.group.name) {
-      case "Admins":
-        if (!client.config.serverRoles.modRoles.forEach((modRole) => msg.member.roles.cache.has(modRole)) || !msg.author.id === client.config.serverRoles.owner) {
-          client.error(`***<@${msg.author.id}>, You don't have permission to use this command***`, msg);
-          msg.delete();
-          return false
-        }
-        break;
-      default:
-        return true; 
-    } 
-  } catch(err) {
-      if (err === "TypeError: Inhibitor \"\" had an invalid result; must be a string or an Inhibition object.") {
-        return;
-      }
-  }
-}); 
-
-client.once("ready", () => {
-  client.user.setPresence({activity: { name: `${client.config.prefix}help ⏯️ https://davidjeong.org` }, status: "online"});  
-
-  log(client, client.config.channels.auditlogs, { embed: { title: "Hooray!", description: "All commands and events work! ✅", color: "GREEN"}});
-});
-
-client 
-    .on("message", (message) => require("./events/message")(client, message))
-    .on("guildMemberAdd", (member) => require("./events/guildMemberAdd")(client, member));
- 
->>>>>>> f3fb4256572e68610ee3a447de3b122fe2707deb
 client.login(client.config.token); 
