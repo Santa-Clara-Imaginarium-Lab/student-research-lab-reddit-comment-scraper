15ead76323961fd3f36c19a23f750eca6c6af8bf
Jason Vu <jvu@scu.edu>
2021-05-07 14:55:29 -0700

    Update announce.js

--- commands/admins/announce.js
+++ commands/admins/announce.js
@@ -1,79 +1,3 @@
-<<<<<<< HEAD
-const { Command } = require("discord.js-commando");
-
-module.exports = class announceCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: "announce",
-      group: "admins",
-      memberName: "announce", 
-      throttling: {
-          usages: 2,
-          duration: 5,
-			},
-      description: "Make a formatted announcement using Embed data",
-      // format: "announce [#channel] [message goes here]\nannounce edit [message id] [new message]\nannounce append [message id] [text to append\nannounce embed [embed JSON]",
-      // examples: [""],
-      args: [
-        {
-          key: "option",
-          prompt: "Please choose a valid option \`msg, embed, append, edit\`",
-          type: "string",
-          oneOf: ["edit", "append", "embed", "msg"],
-        },
-        {
-          key: "id",
-          prompt: "Please provide a message id to edit or mention a channel to send this message to",
-          type: "string",
-          validate: (id) => {
-            if(id.match(/^[0-9]*$/)) {
-              return true;
-            } else {
-                return "Please enter a proper snowflake!"
-            }
-          }
-        },
-        {
-          key: "body",
-          prompt: "Please provide some body text (embed format)",
-          type: "string",
-        },
-      ],
-    });
-  }
-
-  async run(message, { option, id, body }) {
-    const announceChannel = this.client.channels.cache.get(`${id.replace(/</g, "").replace(/>/g, "").replace(/#/g, "")}`);
-    switch (option) {
-      case "edit": 
-          message.channel.messages.fetch(id).then((m) => {
-            m.edit({
-              embed: {
-                description: body, 
-              },
-            });
-          }); 
-        break;
-      case "append": 
-          message.channel.messages.fetch(id).then((m) => {
-            m.edit({
-              embed: {
-                description: m.embeds[0].description + " " + body, 
-              },
-            });
-          }); 
-        break;
-      case "embed":  
-          announceChannel.send({ embed: JSON.parse(body)}); 
-          break;
-      case "msg": 
-          announceChannel.send(body);
- 
-          break;
-    } 
-  } 
-}; 
-=======
 const { Command } = require("discord.js-commando");
 
 module.exports = class announceCommand extends Command {
@@ -180,5 +104,4 @@ module.exports = class announceCommand extends Command {
     }
     message.delete();
   }
-}; 
->>>>>>> f3fb4256572e68610ee3a447de3b122fe2707deb
+};
