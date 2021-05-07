c8189bfbb1fe1bae9859f3c6f3c7b2ade0fdb7e3
Jason Vu <jvu@scu.edu>
2021-05-07 14:55:58 -0700

    Update bot-options.js

--- commands/admins/bot-options.js
+++ commands/admins/bot-options.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { Command } = require("discord.js-commando");
 const childProc = require ("child_process");
 
@@ -46,52 +45,3 @@ module.exports = class botoptionsCommand extends Command {
         } 
     }
 }; 
-=======
-const { Command } = require("discord.js-commando");
-const childProc = require ("child_process");
-
-module.exports = class botoptionsCommand extends Command {
-    constructor(client) {
-        super(client, {
-            name: "botoptions",
-            memberName: "botoptions",
-            description: "Restart, shutdown, or git-pull the bot!",
-            group: "admins",
-            guildOnly: true,  
-            throttling: {
-                usages: 2,
-                duration: 5,
-            },
-            args: [
-                {
-                    key: "option",
-                    prompt: "Please choose a valid option \`restart, shutdown, git-pull\`",
-                    type: "string",
-                    oneOf: ["restart", "shutdown", "git-pull"],
-                }
-            ]
-        });
-    }
-
-	async run ( message, { option }) {   
-        if (option === "restart") {
-            process.exit(); 
-        } else if (option === "shutdown") {
-            this.client.destroy;
-        } else if (option === "git-pull") {
-            childProc.exec("git pull origin master");
-        } else {
-            this.client.error("Please enter either \`restart\`, \`git-pull\`, or \`shutdown\`!");
-        }
-                     
-        const frames = ["□", "□□□□ 25%", "□□□□□□□□ 50", "□□□□□□□□□□□□ 75%", "□□□□□□□□□□□□□□□□ 100%"];
-
-        const msg = await message.channel.send(`${option}ing the bot...`);
-        
-        for (const frame of frames) {
-            setTimeout(() => {}, 4000);
-            await msg.edit({ embed: { description: frame, color: this.client.config.school_color}});
-        } 
-    }
-}; 
->>>>>>> f3fb4256572e68610ee3a447de3b122fe2707deb
