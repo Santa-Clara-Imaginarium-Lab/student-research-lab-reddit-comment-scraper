9cd5e0f3ad7936d57340ee49f3ee42dec5cacbea
Jason Vu <jvu@scu.edu>
2021-05-10 09:41:06 -0700

    fixed announce.js validators

--- commands/admins/announce.js
+++ commands/admins/announce.js
@@ -10,22 +10,20 @@ module.exports = class announceCommand extends Command {
           usages: 2,
           duration: 5,
 			},
-      description: "Make a formatted announcement using Embed data",
-      // format: "announce [#channel] [message goes here]\nannounce edit [message id] [new message]\nannounce append [message id] [text to append\nannounce embed [embed JSON]",
-      // examples: [""],
+      description: "Make a formatted announcement using embed data", 
       args: [
         {
           key: "option",
-          prompt: "Please choose a valid option \`msg, embed, append, edit\`",
+          prompt: "Please choose a valid option \`edit, append, embed\`",
           type: "string",
-          oneOf: ["edit", "append", "embed", "msg"],
+          oneOf: ["edit", "append", "embed"],
         },
         {
           key: "id",
           prompt: "Please provide a message id to edit or mention a channel to send this message to",
           type: "string",
           validate: (id) => {
-            if(id.match(/^[0-9]*$/)) {
+            if(id.match(/^[0-9]*$/) && id.length === 18) {
               return true;
             } else {
                 return "Please enter a proper snowflake!"
@@ -33,42 +31,104 @@ module.exports = class announceCommand extends Command {
           }
         },
         {
+          key: "title",
+          prompt: "Please provide some title text.",
+          type: "string",
+          validate: (title) => {
+            if (title.length < 256) {
+              return true;
+            } else {
+                return "Please enter embed title under 256 characters!"
+            }
+          }
+        },
+
+        {
           key: "body",
-          prompt: "Please provide some body text (embed format)",
+          prompt: "Please provide some body text.",
+          type: "string",
+          validate: (body) => {
+            if (body.length < 2048) {
+              return true;
+            } else {
+                return "Please enter embed description under 2048 characters!";
+            }
+          }
+        },
+        {
+          key: "color",
+          prompt: "Please provide some color.",
           type: "string",
         },
+        {
+          key: "image",
+          prompt: "Please provide an image URL.",
+          type: "string",
+          validate: (image) => {
+            if (image.match(/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i)) {
+              return true;
+            } else {
+                return "Please enter a proper image URL with the listed extensions!";
+            }
+          }
+        },
+        {
+          key: "footer",
+          prompt: "Please provide some footer text.",
+          type: "string",
+          validate: (footer) => {
+            if (footer.length < 2048) {
+              return true;
+            } else {
+                return "Please enter embed footer under 2048 characters!";
+            }
+          }
+        },
+ 
       ],
     });
   }
 
-  async run(message, { option, id, body }) {
-    const announceChannel = this.client.channels.cache.get(`${id.replace(/</g, "").replace(/>/g, "").replace(/#/g, "")}`);
+  async run(message, { option, id, title, body, color, image, footer }) {
     switch (option) {
-      case "edit": 
+      case "edit":
+        try {
           message.channel.messages.fetch(id).then((m) => {
             m.edit({
               embed: {
-                description: body, 
+		title: title,
+                description: body,
+		color: color,
+		footer: footer,
+		image: image
               },
             });
-          }); 
+          });
+        } catch (e) {
+          return this.client.error(e + "Channel not found, you must run in same channel as message!", message);
+        }
         break;
-      case "append": 
+      case "append":
+        try {
           message.channel.messages.fetch(id).then((m) => {
             m.edit({
               embed: {
-                description: m.embeds[0].description + " " + body, 
+                description: m.embeds[0].description + " " + body,
               },
             });
-          }); 
-        break;
-      case "embed":  
-          announceChannel.send({ embed: JSON.parse(body)}); 
-          break;
-      case "msg": 
-          announceChannel.send(body);
- 
-          break;
-    } 
-  } 
-}; 
+          });
+        } catch (e) {
+          return this.client.error("Channel not found, you must run in same channel as message!", message);
+        }
+        break; 
+      case "embed":
+        try {
+          let announceChannel = this.client.channels.cache.get(`${id.replace(/</g, "").replace(/>/g, "").replace(/#/g, "")}`);
+          announceChannel.send({ embed: { title: title, description: body, image: { url: image }, footer: { text: footer }, color: color } });
+        } catch (e) {
+          return this.client.error(e, message);
+        }
+        break;    }
+    message.delete();
+  }
+};
