66bd559676843ccc9f385a28dbc31324d62183e7
Jason Vu <jvu@scu.edu>
2021-06-30 11:46:34 -0700

    fixes and comments for clarity yay

--- modules/getFromReddit.js
+++ modules/getFromReddit.js
@@ -1,13 +1,15 @@
 module.exports.run = async (client) => {
-  const { MessageEmbed } = require(`discord.js`); 
-  const json2csv = require("json2csv");
-  const moment = require("moment");
-  const snoowrap = require("snoowrap");
-  const fs = require("fs");
-  const request = require("request");
-  const entities = require("entities");
-  const validUrl = require("valid-url");
-  const { log } = require(`../functions/log.js`);
+  //packages, external libraries, and functions I need to properly garner information
+  const { MessageEmbed } = require(`discord.js`); // powerful library for interacting with the Discord API
+  const json2csv = require("json2csv"); // convert json to csv
+  const moment = require("moment"); // JavaScript date library for parsing, validating, manipulating, and formatting datess
+  const snoowrap = require("snoowrap"); // fully-featured JavaScript wrapper that provides a simple interface to access every reddit API endpoint
+  const fs = require("fs"); // provides a lot of very useful functionality to access and interact with the file system
+  const request = require("request"); // simplest way to make http calls
+  const entities = require("entities"); // decodes html entities (e.g. &amp; becomes &, &quot; becomes ", &lt becomes <, &gt; becomes >)
+  const validUrl = require("valid-url"); // checks if a url is valid
+  const { log } = require(`../functions/log.js`); // allows message embed to be sent to channel
+
   const botReady = true; 
   
   setInterval(async () => {
@@ -19,8 +21,8 @@ module.exports.run = async (client) => {
           url: `https://reddit.com/r/${client.config.api.subreddit.name}/${getSortBy}.json?limit=10`, //every time, randomize between new, random, top, or rising for more varied posts
           json: true,
         }, (error, response, body) => {
-          if (!error && response.statusCode === 200) {
-            for (const post of body.data.children.reverse()) {
+          if (!error && response.statusCode === 200) { // Means your request was successful and the server responded with the data you were requesting. Sometimes, you might want to use this information to make decisions in your code: if response
+            for (const post of body.data.children.reverse()) { // runs for loop through elements nested under body.data.children (json object) for the subreddit post
               const lastTimestamp = post.data.created_utc;
               if (lastTimestamp <= post.data.created_utc) {
   
@@ -38,39 +40,39 @@ module.exports.run = async (client) => {
   
                 // Pass in a username and password for script-type apps.
                 const redditFetch = new snoowrap({
-                  userAgent: client.config.api.subreddit.user_agent,
-                  clientId: client.config.api.subreddit.client_id,
-                  clientSecret: client.config.api.subreddit.client_secret,
-                  username: client.config.api.subreddit.username,
-                  password: client.config.api.subreddit.password
+                  userAgent: client.config.api.subreddit.user_agent, // A user agent header is a string of text that is sent with HTTP requests to identify the program making the request (the program is called a "user agent"). Web browsers commonly send these in order to identify themselves (so the server can tell if you're using Chrome or Firefox, for example).
+                  clientId: client.config.api.subreddit.client_id, // client id needed to access Reddit’s API as a script application
+                  clientSecret: client.config.api.subreddit.client_secret, // client secret needed to access Reddit’s API as a script application
+                  username: client.config.api.subreddit.username, // my reddit username 
+                  password: client.config.api.subreddit.password // my reddit password
                 })
     
                 const redditPost = new MessageEmbed()
                   .setColor(client.config.school_color)
-                  .setAuthor(`${post.data.subreddit_name_prefixed} | ${post.data.subreddit_subscribers} Subscribers`, client.user.displayAvatarURL()) //displays author's username
-                  .setTitle(`${post.data.link_flair_text ? `[${post.data.link_flair_text}] ` : ""}${entities.decodeHTML(post.data.title.length > 256 ? post.data.title.slice(0, 256).concat("...") : post.data.title)}`) // gets the post's title
-                  .setURL(`https://redd.it/${post.data.id}`) //attaches URL of reddit post here
+                  .setAuthor(`${post.data.subreddit_name_prefixed} | ${post.data.subreddit_subscribers} Subscribers`, client.user.displayAvatarURL()) // displays author's username
+                  .setTitle(`${post.data.link_flair_text ? `[${post.data.link_flair_text}] ` : ""}${entities.decodeHTML(post.data.title.length > 256 ? post.data.title.slice(0, 256).concat("...") : post.data.title)}`) // gets the post's title; anything over 256 is appended with ellipses
+                  .setURL(`https://redd.it/${post.data.id}`) // attaches URL of reddit post here
                   .setDescription(`${post.data.is_self ? entities.decodeHTML(post.data.selftext.length > 2048 ? post.data.selftext.slice(0, 2048).concat("...") : post.data.selftext) : ""}`) // posts descriptions; anything over 2048 is appended with ellipses            
                   .setThumbnail(validUrl.isUri(post.data.thumbnail) ? entities.decodeHTML(post.data.thumbnail) : null)
                   .setFooter(`[${post.data.is_self ? "Self Post" : "Link Post"} by /u/${post.data.author}] | [${post.data.ups} 👍] | [${post.data.downs} 👎 ] | [${post.data.num_comments} 📃]`) // determines if author's post is a self post or link post
-                  .setTimestamp(new Date(post.data.created_utc * 1000)) //gets time relative to one's time zone (e.g. PST) when the author posted
+                  .setTimestamp(new Date(post.data.created_utc * 1000)) // gets time relative to one's time zone (e.g. PST) when the author posted
                 
                 
                 setInterval(async () => {
-                  //gather submission comment thread's replies and outputs content of each comment assuming there is at least one comment
+                  // gather submission comment thread's replies and console logs content of each comment assuming there is at least one comment
                   redditFetch.getSubmission(post.data.id).expandReplies()
-                  .then(thread => {  // comment format: [ [author flair text] [comment body] by (comment author name) posted on (comment date) from (comment permalink)]
-                    thread.comments.forEach((comment) => console.log(`[ [${comment.body.author_flair_text || "no comment body author flair"}] ${comment.body}] by ${comment.author.name} posted on ${moment(post.data.created_utc * 1000).format("YYYY-DD-MM")} from ${comment.permalink}`))
+                  .then(thread => {  // comment format: [ [author flair text] [comment body] by (comment author name) posted on (comment date) from (comment url)]
+                    thread.comments.forEach((comment) => console.log(`[ [${comment.body.author_flair_text || "no comment body author flair"}] ${comment.body}] by ${comment.author.name} posted on ${moment(post.data.created_utc * 1000).format("YYYY-DD-MM")} from ${comment.url}`))
                   }).catch(err => console.log(err));  
-                }, 5000 )
+                }, 5000 ) // 5 second timeout to avoid API collisions
 
-                //gets post data id of the post and names it as such appended by ".csv" in my /redditPosts/ file directory on my Raspberry Pi   
+                // gets post data id of the post and names it as such appended by ".csv" in my /redditPosts/ file directory on my Raspberry Pi   
                 // get file name to be this format: [date_post-flair_post-data-id.csv]
   
-                fs.writeFileSync(`./redditPosts/${moment(post.data.created_utc * 1000).format("YYYY-DD-MM")}_${post.data.link_flair_text || "no_flair"}_${post.data.id}.csv`, json2csv.parse(redditPost), {"encoding": "utf-8"});  //writes to text file ; made this a csv file for better file readability and organization
-                redditPost.attachFiles(`./redditPosts/${moment(post.data.created_utc * 1000).format("YYYY-DD-MM")}_${post.data.link_flair_text || "no_flair"}_${post.data.id}.csv`); //attaches file to each respective redditPost embed that's sent every 10 minutes
+                fs.writeFileSync(`./redditPosts/${moment(post.data.created_utc * 1000).format("YYYY-DD-MM")}_${post.data.link_flair_text || "no_flair"}_${post.data.id}.csv`, json2csv.parse(redditPost), {"encoding": "utf-8"});  // writes to text file ; made this a csv file for better file readability and organization
+                redditPost.attachFiles(`./redditPosts/${moment(post.data.created_utc * 1000).format("YYYY-DD-MM")}_${post.data.link_flair_text || "no_flair"}_${post.data.id}.csv`); // attaches file to each respective redditPost embed that's sent every 10 minutes
   
-                log(client, client.config.channels.reddit, redditPost); //sends to the #reddit-feed channel  
+                log(client, client.config.channels.reddit, redditPost); // sends to the #reddit-feed channel  
               } 
             }
           } else {
--- modules/getFromTwitter.js
+++ modules/getFromTwitter.js
@@ -5,23 +5,30 @@ module.exports.run = async (client) => {
     setInterval(async () => {
         const Twit = require("twit"); 
 
-        const TwitterInstance = new Twit({
+        const TwitterInstance = new Twit({ //authenticating with secret keys provided with secret keys from developer account
             consumer_key:         client.config.api.twitter.consumer_key,
             consumer_secret:      client.config.api.twitter.consumer_secret,
             access_token:         client.config.api.twitter.access_token,
             access_token_secret:  client.config.api.twitter.access_token_secret
         }); 
+
+        const params = {
+            q: "Pat Bev", // search term
+            count: 12 // how many tweets back
+        }
     
-        TwitterInstance.get("search/tweets", { q: "Pat Bev", count: 10 }, function (err, data, response) {
+        TwitterInstance.get("search/tweets", params, function (err, data, response) {
             if (!err && response.statusCode === 200) { 
                 const redditPost = new MessageEmbed()
-                .setDescription(data.statuses[0].text)
-                .setTimestamp(data.statuses[0].created_at)
+                .setDescription(data.statuses[0].text) // gets text
+                .setFooter(data.statuses[0].created_at) //gets created date
 
-                log(client, client.config.channels.twitter, redditPost)
+                log(client, client.config.channels.twitter, redditPost) // sends messagee embed to #twitter-feed
             } else {
                 console.log(err);
             }
         });
-    }, 600 * 1000); //gets 10 posts every 10 minutes
+    }, 600 * 1000); // gets 10 posts every 10 minutes
 }
+
+// wouldn't be flagged for being bot  and/or hacker
